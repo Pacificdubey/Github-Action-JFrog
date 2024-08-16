30901435472d8ff026f96b84aab1d9de941ed175
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-08-16 15:23:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = "JFrog Integration Testing",
   version='0.1.0',
   package=find_packages(),
-  py_modules=['']
+  py_modules=[''],
   python_requires='>=3.6',
   author='Prashant Dubey',
   author_email='pacific.prashantdubey@gmail.com',
