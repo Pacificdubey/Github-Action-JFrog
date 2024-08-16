785654df58f2c68f50fd6f500a08cb70c6f95d85
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-08-16 15:14:32 +0530

    Update app.py [skip ci]]

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-import requests
-
-def main():
-  response = requests.get("https://api.github.com")
-  if response.status_code == 200:
-    print("Github is accessible")
-  else:
-    print("Failed to reach Github API")
+def hello():
+  print("Hello, World")
 
 if __name__ == "__main__":
-  main()
+  hello()
