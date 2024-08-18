780529492cd443398483e887379edad02cb623ff
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-08-18 16:19:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-  name = "JFrog Integration Testing",
-  version='0.1.0',
-  package=find_packages(),
-  py_modules=[''],
-  python_requires='>=3.6',
-  author='Prashant Dubey',
-  author_email='pacific.prashantdubey@gmail.com',
-  url='https://github.com/Pacificdubey/Github-Action-JFrog',
+    name="my-python-app",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Flask==2.1.1",
+    ],
 )
+
