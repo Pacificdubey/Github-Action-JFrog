ef9672ae2d72a6d9126f466ca5b1d5542bce58fb
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-08-18 16:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
-  print("Hello, World")
+    return "Hello, World!"
 
 if __name__ == "__main__":
-  hello()
+    app.run(host="0.0.0.0", port=80)
+
