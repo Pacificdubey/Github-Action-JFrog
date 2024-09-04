90d054593adc6dafbdc342ffe52ae18f2368afbf
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-09-04 07:14:24 +0530

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -7,9 +7,6 @@ module.exports = {
 plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-    ['@semantic-release/exec', {
-      prepareCmd: 'echo ${nextRelease.version} > VERSION.txt'
-    }]
+    '@semantic-release/github'
   ]
 };
