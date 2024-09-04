dbedc5b871800cc6c88b1c8221b07007f690262e
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-09-04 08:23:45 +0530

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  branches: [
+    'main',
+    { name: 'develop', prerelease: true },
+    { name: 'feature/testing', prerelease: 'beta' }  // Automatically uses the branch name as the identifier
+  ],
+plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator'
+  ]
+};
