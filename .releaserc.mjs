5ebc9d51b09eb6e3c9f135e621702dcc447b1ebe
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-09-07 14:56:59 +0530

    Update .releaserc.mjs

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -2,16 +2,16 @@ import semanticRelease from "semantic-release";
 
 semanticRelease({
   branches: [
-    "main",                // Main production branchs
-    { name: "develop", prerelease: true },  // Develop branch for pre-releases
-    { name: "release/*", prerelease: true } // Release branches for pre-releases
+    "main",
+    { name: "develop", prerelease: true },
+    { name: "release/*", prerelease: true },
   ],
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     ["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/git"],
-    ["@semantic-release/github"]
+    ["@semantic-release/github"] // Ensure the GitHub plugin comes after git and other steps
   ]
 }).then(({ lastRelease, nextRelease }) => {
   console.log(`Last release: ${lastRelease?.version}`);
