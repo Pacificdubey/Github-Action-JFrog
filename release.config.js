dd0cad80d84603c6038c6c574475338fd65e14c0
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-09-07 22:01:49 +0530

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,10 +1,8 @@
-import semanticRelease from "semantic-release";
-
-semanticRelease({
+module.exports = {
   branches: [
-     'main', // Main branch for production releases
-    { name: 'develop', prerelease: true }, // Develop branch for prereleases
-    { name: 'release/*', prerelease: true } // Wildcard for release branches
+    "main", // Main branch for production releases
+    { name: "develop", prerelease: true }, // Develop branch for prereleases
+    { name: "release/*", prerelease: true } // Wildcard for release branches
   ],
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
@@ -13,10 +11,4 @@ semanticRelease({
     ["@semantic-release/git"],
     ["@semantic-release/github"]
   ]
-}).then(({ lastRelease, nextRelease }) => {
-  console.log(`Last release: ${lastRelease?.version}`);
-  console.log(`Next release: ${nextRelease?.version}`);
-}).catch(err => {
-  console.error("The automated release failed with %O", err);
-});
-
+};
